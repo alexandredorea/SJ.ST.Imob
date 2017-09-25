a13e13982d2ecec64841b07f6462b1d28b17b141
dekionbr <silveira.julio@gmail.com>
2017-09-25 02:16:44 -0300

    task em] microserviço

--- SetorConsumer/Program.cs
+++ SetorConsumer/Program.cs
@@ -3,13 +3,15 @@ using SJ.ST.Imob.Consumer;
 using SJ.ST.Imob.Core;
 using SJ.ST.Imob.Repository;
 using System;
+using System.Threading.Tasks;
 
 namespace SetorConsumer
 {
     static class Program
     {
-        static readonly Container container; 
-        static Program() {
+        static readonly Container container;
+        static Program()
+        {
             container = new Container();
 
             // 2. Configure the container (register)
@@ -22,12 +24,14 @@ namespace SetorConsumer
         static void Main(string[] args)
         {
             var repository = container.GetInstance<Repository<Setor>>();
-
-            Consumer.Get<Setor>(repository);
-            Consumer.GetList<Setor>(repository);
-            Consumer.Insert<Setor>(repository);
-            Consumer.Update<Setor>(repository);
-            Consumer.Delete<Setor>(repository);
+            Task.Factory.StartNew(() =>
+            {
+                Consumer.Get<Setor>(repository);
+                Consumer.GetList<Setor>(repository);
+                Consumer.Insert<Setor>(repository);
+                Consumer.Update<Setor>(repository);
+                Consumer.Delete<Setor>(repository);
+            });
 
             Console.WriteLine("Setor Hello World!");
         }
